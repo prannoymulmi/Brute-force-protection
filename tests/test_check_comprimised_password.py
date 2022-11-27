693b7b9589444f7f5ffd06c3b9aeccd35de2d6d4
prannoy <prannoy.mulmi@gmail.com>
2022-11-27 16:25:33 +0100

    Added comrpomised passwords

---
+++ tests/test_check_comprimised_password.py
@@ -0,0 +1,30 @@
+from unittest.mock import MagicMock
+
+from utils.check_comprimised_password import check_compromised_password
+import requests
+
+from utils.jwt_utils import get_project_root
+
+
+def test_check_compromised_password_when_breached_password_is_given_then_true_is_returned():
+    with open(f"{get_project_root()}/tests/hashes_for_test.txt") as f:
+        s = f.read()
+        # mock the response
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.content = s
+        requests.get.return_value = mock_response
+        result = check_compromised_password("test1234")
+        assert result
+
+
+def test_check_compromised_password_when_non_breached_password_is_given_then_false_is_returned():
+    with open(f"{get_project_root()}/tests/hashes_for_test.txt") as f:
+        s = f.read()
+        # mock the response
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.content = s
+        requests.get.return_value = mock_response
+        result = check_compromised_password("Very$GoodPassQord$3!%")
+        assert result == False
